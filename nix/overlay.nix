9d7a89f606fbd64660a5ef258cf6141586738493
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-05-14 17:09:28 +0200

    build terminfo dirs into the statically linked ncurses

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -20,29 +20,49 @@ with final.haskell.lib;
         if final.stdenv.hostPlatform.isMusl
         then
           overrideCabal pkg
-            (old: {
-              configureFlags = (old.configureFlags or [ ]) ++ [
-                "--ghc-option=-optl=-static"
-                # Static
-                "--extra-lib-dirs=${final.gmp6.override { withStatic = true; }}/lib"
-                "--extra-lib-dirs=${final.libffi.overrideAttrs (_: { dontDisableStatic = true; })}/lib"
-                "--extra-lib-dirs=${final.zlib.static}/lib"
-                "--extra-lib-dirs=${final.ncurses.override { enableStatic = true; }}/lib" # for -ltinfo
-              ];
-              enableSharedExecutables = false;
-              enableSharedLibraries = false;
+            (old:
+              let
+                # Until https://github.com/NixOS/nixpkgs/pull/311411
+                terminfoDirs = final.lib.concatStringsSep ":" [
+                  "/etc/terminfo" # Debian, Fedora, Gentoo
+                  "/lib/terminfo" # Debian
+                  "/usr/share/terminfo" # upstream default, probably all FHS-based distros
+                  "/run/current-system/sw/share/terminfo" # NixOS
+                ];
+                staticNcurses = (
+                  (final.ncurses.override {
+                    enableStatic = true;
+                  })
+                ).overrideAttrs
+                  (old: {
+                    configureFlags = (old.configureFlags or [ ]) ++ [
+                      "--with-terminfo-dirs=${terminfoDirs}"
+                    ];
+                  });
+              in
+              {
+                configureFlags = (old.configureFlags or [ ]) ++ [
+                  "--ghc-option=-optl=-static"
+                  # Static
+                  "--extra-lib-dirs=${final.gmp6.override { withStatic = true; }}/lib"
+                  "--extra-lib-dirs=${final.libffi.overrideAttrs (_: { dontDisableStatic = true; })}/lib"
+                  "--extra-lib-dirs=${final.zlib.static}/lib"
+                  "--extra-lib-dirs=${staticNcurses}}/lib" # for -ltinfo
+                ];
+                enableSharedExecutables = false;
+                enableSharedLibraries = false;
 
-              postInstall = (old.postInstall or "") + ''
-                for b in $out/bin/*
-                do
-                  if ldd "$b"
-                  then
-                    echo "ldd succeeded on $b, which may mean that it is not statically linked"
-                    exit 1
-                  fi
-                done
-              '';
-            })
+                postInstall = (old.postInstall or "") + ''
+                  for b in $out/bin/*
+                  do
+                    if ldd "$b"
+                    then
+                      echo "ldd succeeded on $b, which may mean that it is not statically linked"
+                      exit 1
+                    fi
+                  done
+                '';
+              })
         else pkg;
     in
     builtins.mapAttrs
